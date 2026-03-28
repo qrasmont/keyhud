bd1962018e00c8de80edce84bba1823af5a24e02
Quentin <quentin@rasmont.com>
2026-03-28 22:09:36 +0100

    Display key press/release

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use iced::{Application, Background, Color, Element, Length::Fill, Program, Task, Theme};
-use rdev::{listen, Event, EventType};
+use iced::{
+    Application, Background, Color, Element,
+    Length::Fill,
+    Program, Subscription, Task, Theme,
+    futures::{SinkExt, Stream, StreamExt, channel::mpsc},
+    stream,
+};
+use rdev::{Event, EventType, Key, grab};
 
 pub fn main() -> iced::Result {
     application().run()
@@ -7,6 +13,7 @@ pub fn main() -> iced::Result {
 
 fn application() -> Application<impl Program<Message = Message, Theme = Theme>> {
     iced::application(App::new, App::update, App::view)
+        .subscription(App::subscription)
         .title("keyhud")
         .transparent(true)
         .decorations(false)
@@ -15,28 +22,40 @@ fn application() -> Application<impl Program<Message = Message, Theme = Theme>> 
 }
 
 #[derive(Default)]
-struct App;
+struct App {
+    keys: Vec<String>,
+}
 
 #[derive(Debug, Clone)]
-enum Message {}
+enum Message {
+    KeyPressed(Key),
+    KeyReleased(Key),
+}
 
 impl App {
     fn new() -> (App, Task<Message>) {
-        listen(|event: Event| {
-            if let EventType::KeyPress(key) = event.event_type {
-                println!("Key pressed: {key:?}");
-            }
-        }).unwrap();
-
         (App::default(), Task::none())
     }
 
-    fn update(_state: &mut App, _message: Message) -> Task<Message> {
+    fn update(state: &mut App, message: Message) -> Task<Message> {
+        match message {
+            Message::KeyPressed(key) => state.keys.push(format!("p {key:?}")),
+            Message::KeyReleased(key) => state.keys.push(format!("d {key:?}")),
+        }
         Task::none()
     }
 
     fn view(&self) -> Element<'_, Message> {
-        iced::widget::container(iced::widget::Space::new())
+        let entries = self
+            .keys
+            .iter()
+            .rev()
+            .map(|line| iced::widget::text(line).into())
+            .collect::<Vec<_>>();
+
+        let content = iced::widget::scrollable(iced::widget::column(entries).spacing(4));
+
+        iced::widget::container(content)
             .width(Fill)
             .height(Fill)
             .style(|_theme| iced::widget::container::Style {
@@ -59,4 +78,35 @@ impl App {
             },
         )
     }
+
+    fn subscription(_state: &App) -> Subscription<Message> {
+        Subscription::run(global_key_work)
+    }
+}
+
+fn global_key_work() -> impl Stream<Item = Message> {
+    stream::channel(100, async |mut output| {
+        let (tx, mut rx) = mpsc::unbounded::<Event>();
+
+        std::thread::spawn(move || {
+            grab(move |event: Event| -> Option<Event> {
+                let _ = tx.unbounded_send(event.clone());
+                Some(event)
+            })
+        });
+
+        loop {
+            if let Some(event) = rx.next().await {
+                let msg = match event.event_type {
+                    EventType::KeyPress(key) => Some(Message::KeyPressed(key)),
+                    EventType::KeyRelease(key) => Some(Message::KeyReleased(key)),
+                    _ => None,
+                };
+
+                if let Some(msg) = msg {
+                    let _ = output.send(msg).await;
+                }
+            }
+        }
+    })
 }
